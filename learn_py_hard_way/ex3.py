4042a43240a108f94fa69494a5fa626323d35401
Jonathan Chin <jonathankchin@gmail.com>
2019-11-24 15:52:50 -0500

    updated exercise

--- learn_py_hard_way/ex3.py
+++ learn_py_hard_way/ex3.py
@@ -1,23 +1,44 @@
+# Line prints out "I will now count my chickens"
 print("I will now count my chickens:")
 
+# Line prints out "Hens", divides 30 by 6 and then adds 5 to 25 for 30
 print("Hens", 25 + 30 / 6)
+
+# Line prints out "Roosters", multiples 25 and 3, modulo 75 by 4 to 3, then 100 subtact 3
 print("Roosters", 100 - 25 * 3 % 4)
 
+# Line prints out "Now I will count the eggs"
 print("Now I will count the eggs:")
 
+# Divide & Multiply
+  # 3 + 2 + 1 - 5 + 0 - 0.25 + 6
+# Addition & Subtraction
+  # 6 - 5 + 0 - 0.25 + 6
+  # 1 + 0 - 0.25 + 6
+  # 1 - 0.25 + 6
+  # 0.75 + 6
+  # 6.75
 print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
 
+# Line prints "Is it true that 3 + 2 < 5 - 7?"
 print("Is it true that 3 + 2 < 5 - 7?")
-
+# Line prints "False"
 print(3 + 2 < 5 - 7)
 
+# Line prints "What is 3 + 2? 5"
 print("What is 3 + 2?", 3 + 2)
+# Line prints "What is 5 - 7? -2"
 print("What is 5 - 7?", 5 - 7)
 
+# Line prints "Oh, that's why it's False"
 print("Oh, that's why it's False.")
 
+# Line prints "How about some more."
 print("How about some more.")
 
+# Line prints "Is it greater? True"
 print("Is it greater?", 5 > -2)
+# Line prints "Is it greater or equal? True"
 print("Is it greater or equal?", 5 >= -2)
+# Line prints "Is it less or equal? False"
 print("Is it less or equal?", 5 <= -2)
