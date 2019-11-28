1a287c2e0643fb0fc97bbf2efc8c6e38c5fd8bf1
Jonathan Chin <jonathankchin@gmail.com>
2019-11-28 14:06:51 -0500

    init commit of extercises

---
+++ learn_py_hard_way/ex10.py
@@ -0,0 +1,62 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\a \\ cat."
+
+fat_cat_double_quote = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+fat_cat_single_quote = '''
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+'''
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat_double_quote)
+print(fat_cat_single_quote)
+
+###
+
+# backslash
+print("backslash: \\")
+
+# single quote
+print("single quote: \'")
+
+# double quote
+print("double quote: \"")
+
+# ASCII bell
+print("ASCII bell: \a")
+
+# ASCII backspace
+print("ASCII backspace: \b")
+
+# ASCII formfeed 
+print("ASCII formfeed: \f")
+
+# ASCII linefeed
+print("ASCII linefeed: \n")
+
+# Character named name in the unicode database
+# name = "My Name"
+# print(\N{name})
+
+# Carriage return
+print("Carriage return: \r")
+
+# Horizontal tab
+print("Horizontal tab: \t")
+
+# \uxxxx character with 16-bit hex value xxxx
+# print("\uxxxx: \uxxxx")
+
+# \Uxxxxxxxx
+# print("\Uxxxxxxx")
