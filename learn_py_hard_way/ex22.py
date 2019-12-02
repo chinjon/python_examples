3d8c329bbd5aa76e58da6a1d608ae566e85ba220
Jonathan Chin <jonathankchin@gmail.com>
2019-12-01 23:54:57 -0500

    init commit of exercise

---
+++ learn_py_hard_way/ex22.py
@@ -0,0 +1,9 @@
+# Learnings Thus Far
+
+# def
+# def is a keyword reserved to declare a function
+# def(parameters):
+
+
+# import
+# import is a keyword reserved for including modules into a script
