07d0966cf0b1d4a1390d6e9cd180ef8e1ca1f6c1
Jonathan Chin <jonathankchin@gmail.com>
2019-12-04 20:32:40 -0500

    init commit of exercise

---
+++ learn_py_hard_way/ex37.py
@@ -0,0 +1,21 @@
+# Symbols
+
+# KEYWORDS
+
+# and: logical and
+# print(True and False == False)
+
+# as: part of the with-as statement
+# print(with X as Y: pass)
+
+# assert
+# assert that something is true
+# assert False, "Error!"
+
+# break
+# stops loop
+# while True: break
+
+# class
+# define a class
+# class Person(object)
