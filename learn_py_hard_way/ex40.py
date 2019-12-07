8aa49388aa7c40729da633dcfc53dec64b86de2e
Jonathan Chin <jonathankchin@gmail.com>
2019-12-07 17:48:02 -0500

    modified exercise

--- learn_py_hard_way/ex40.py
+++ learn_py_hard_way/ex40.py
@@ -1,3 +1,8 @@
 import ex40_my_stuff
 
+thing = ex40_my_stuff.MyStuff()
+
 ex40_my_stuff.apple()
+print(ex40_my_stuff.tangerine)
+
+thing.apple()
--- learn_py_hard_way/ex40_my_stuff.py
+++ learn_py_hard_way/ex40_my_stuff.py
@@ -1,3 +1,12 @@
 def apple():
   print("I AM APPLES!")
   
+tangerine = "Living reflection of a dream."
+
+class MyStuff(object):
+	
+  def _init_(self):
+    self.tangerine = "And now a thousand years between."
+	
+  def apple(self):
+    print("I AM CLASSY APPLES!")
