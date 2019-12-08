9e0bf6c35b3f08a80af3323e9fcf134ce60a2d34
Jonathan Chin <jonathankchin@gmail.com>
2019-12-07 20:58:02 -0500

    updated exercise

--- learn_py_hard_way/ex40.py
+++ learn_py_hard_way/ex40.py
@@ -1,8 +1,23 @@
 import ex40_my_stuff
 
+class Song(object):
+  
+  def _init_(self, lyrics):
+    self.lyrics = lyrics
+    
+  def sing_me_a_song(self):
+    for line in self.lyrics:
+      print(line)
+
 thing = ex40_my_stuff.MyStuff()
 
 ex40_my_stuff.apple()
 print(ex40_my_stuff.tangerine)
 
 thing.apple()
+
+happy_bday = Song(["Happy birthday to you", "I don't want to get sued", "So I'll stop right there"])
+
+bulls_on_parade = Song(["They rally around the family", "With pockets full of shells"])
+
+happy_bday.sing_me_a_song()
