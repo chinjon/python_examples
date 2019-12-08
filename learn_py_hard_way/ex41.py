a2aaea948f045d84ae69da4c351d33269fb89bf6
Jonathan Chin <jonathankchin@gmail.com>
2019-12-08 12:49:55 -0500

    updated exercises

--- learn_py_hard_way/ex41.py
+++ learn_py_hard_way/ex41.py
@@ -6,9 +6,9 @@ WORD_URL = "http://learncodethehardway.org/words.txt"
 WORDS = []
 
 PHRASES = {
-  "class %%%{%%%}:
-    "Make a class named %%% that is -a %%%."
-  "class %%%{object}:\n\tdef __init__{self, ***}:
+  "class %%%{%%%}":
+    "Make a class named %%% that is -a %%%.",
+  "class %%%{object}:\n\tdef __init__{self, ***}":
     "class %%% has-a __init__ that takes self and *** params.",
   "class %%%{object}:\m\tdef ***{self, @@@}":
     "class %%% has-a function *** that takes self and @@@ params.",
@@ -20,4 +20,51 @@ PHRASES = {
     "From *** get the *** attribute and set it to '***'."
 }
 
-if len(sys.argv) == 2 and sys.argv[1] == "english"
+if len(sys.argv) == 2 and sys.argv[1] == "english":
+  PHRASE_FIRST = True
+else:
+  PHRASE_FIRST = False
+  
+for word in urlopen(WORD_URL).readlines():
+  WORDS.append(str(word.strip(), encoding="utf8"))
+
+def convert(snippet, phrase):
+  class_names = [w.capitalize() for w in random.sample(WORDS, snippet.count("%%%"))]
+  other_names = random.sample(WORDS, snippet.count("***"))
+  results = []
+  param_names = []
+  
+  for i in range(0, snippet.count("@@@")):
+    param_count = random.randint(1,3)
+    param_names.append(', '.join(random.sample(WORDS, param_count)))
+    
+  for sentence in snippet, phrase:
+    result = sentence[:]
+    
+    for word in class_names:
+      result = result.replace("%%%", word, 1)
+      
+    for word in other_names:
+      result = result.replace("***", word, 1)
+      
+    results.append(result)
+    
+  return results
+  
+  try:
+    while True:
+      snippets = list(PHRASES.keys())
+      random.shuffle(snippets)
+      
+      for snippet in snippets:
+        phrase = PHRASES[snippet]
+        question, answer = convert(snippet, phrase)
+        if PHRASE_FIRST:
+          question, answer = answer, question
+          
+          print(question)
+          
+          input("> ")
+          print(f"ANSWER: {answer}\n\n")
+  except EOFError:
+    print("\nBye")
