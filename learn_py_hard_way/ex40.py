78311f41f9addafbcd6f0735ddf132b665c17f61
Jonathan Chin <jonathankchin@gmail.com>
2019-12-08 00:30:13 -0500

    updated exercise

--- learn_py_hard_way/ex40.py
+++ learn_py_hard_way/ex40.py
@@ -1,8 +1,8 @@
 import ex40_my_stuff
 
 class Song(object):
-  
-  def _init_(self, lyrics):
+	
+  def __init__(self, lyrics):
     self.lyrics = lyrics
     
   def sing_me_a_song(self):
@@ -16,8 +16,12 @@ print(ex40_my_stuff.tangerine)
 
 thing.apple()
 
+# creates happy_birthday
+# inherits all of methods and properties of Song()
 happy_bday = Song(["Happy birthday to you", "I don't want to get sued", "So I'll stop right there"])
 
 bulls_on_parade = Song(["They rally around the family", "With pockets full of shells"])
 
 happy_bday.sing_me_a_song()
+
+bulls_on_parade.sing_me_a_song()
