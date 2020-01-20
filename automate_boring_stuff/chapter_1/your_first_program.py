f9c7c6ca877f1a1d5be52c7ea02676d620f9ff98
Jonathan Chin <jonathankchin@gmail.com>
2020-01-20 14:18:32 -0500

    updated with comment

--- automate_boring_stuff/chapter_1/your_first_program.py
+++ automate_boring_stuff/chapter_1/your_first_program.py
@@ -2,10 +2,10 @@
 
 print('Hello world!')
 print('What is your name?')
-myName = input()
+myName = input()  # inpu asks for name
 print('It is good to mee you, ' + myName)
 print('The length of your name is:')
-print(len(myName))
+print(len(myName))  # prints out length of name from input
 print('What is your age?')
-myAge = input()
-print('You will be ' + str(int(myAge) + 1) + ' in a year.')
+myAge = input()  # asks for age
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')  # prints out age next year
