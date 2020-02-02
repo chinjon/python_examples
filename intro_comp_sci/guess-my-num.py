4c4edca7b1ce5b1e4326b12778898d23732af690
Jonathan Chin <jonathankchin@gmail.com>
2020-02-02 12:03:34 -0500

    updated program

---
+++ intro_comp_sci/guess-my-num.py
@@ -0,0 +1,21 @@
+
+low = 0 
+high = 100
+correct = False
+guess = 0
+userIndicator = ''
+print('Please think of a number between 0 and 100!')
+
+while True:
+  answer = (high+low)//2
+  print('Is your secrect number ' + str(answer) + '?')
+  userIndicator = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.")
+  
+  if userIndicator == 'h':
+    high = answer
+  elif userIndicator == 'l':
+    low = answer
+  elif userIndicator =='c':
+    break
+  else:
+    print('Sorry, I did not understand your input.')
